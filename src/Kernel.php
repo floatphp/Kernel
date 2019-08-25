7574c442526c58dc0aa5f47304d891913adf2a9e
JIHAD SINNAOUR <j.sinnaour.official@gmail.com>
2019-08-25 13:57:24 +0100

    Create Kernel.php

---
+++ src/Kernel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author    : JIHAD SINNAOUR
+ * @package   : FloatPHP
+ * @subpackage: Kernel Component
+ * @version   : 1.0.0
+ * @category  : PHP framework
+ * @copyright : (c) JIHAD SINNAOUR <mail@jihadsinnaour.com>
+ * @link      : https://www.floatphp.com
+ * @license   : MIT License
+ */
+
+namespace floatPHP\Kernel;
+
+class Kernel
+{
+	public function __construct()
+	{
+		new \floatPHP\Kernel\Module;
+		new \floatPHP\Kernel\Middleware;
+	}
+}
